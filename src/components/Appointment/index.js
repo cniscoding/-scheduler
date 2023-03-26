61fd6e1ff56062e339d37e50b59dba7f6e06cdc0
cniscoding <asdfa.com>
2023-03-25 21:10:40 -0700

    add logic to appointment index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,36 @@ import "./styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import { useVisualMode } from "hooks/useVisualMode";
+
+//mode constants
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
-  // const show = (props.interview) ? Show : Empty;
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time}> </Header>
-      {(props.interview) ?
+      {/* {(props.interview) ?
         <Show interviewer={props.interview.interviewer.name} student={props.interview.student} /> :
         <Empty />
-      }
-
+      } */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE) }/>}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {/* will add to loater as per instructions */}
+      {mode === CREATE && <Form />} 
 
     </article>
 
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+// create function that takes in an initial mode
+export function useVisualMode(initial) {
+
+  //set the mode state to initial
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (newMode !== mode) {
+      setMode(newMode);
+      (replace
+        ? history[history.length - 1] = newMode
+        : history.push(newMode)
+      );
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+      setHistory([...history])
+    }
+  }
+  // return an object with a mode property
+  return { mode, transition, back }
+}
