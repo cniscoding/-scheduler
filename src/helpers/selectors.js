82a49de081772e34abcc6254adf93747403d1760
cniscoding <asdfa.com>
2023-03-26 21:30:34 -0700

    refactor helper fn selector.js

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,26 +1,49 @@
-export function getAppointmentsForDay(state, day) {
-  if (state.days.length === 0) {
+// export function getAppointmentsForDay(state, day) {
+//   if (state.days.length === 0) {
+//     return []
+//   }
+//   const arrAppointments = state.days.filter(dayFilter => dayFilter.name === day)
+//   if (arrAppointments.length === 0) {
+//     return []
+//   }
+//   const appointments = arrAppointments[0].appointments.map(x => state.appointments[x])
+//   return appointments
+// }
+
+// export function getInterviewersForDay (state, day) {
+//   if (state.days.length === 0) {
+//     return []
+//   }
+//   const arrAppointments = state.days.filter(dayFilter => dayFilter.name === day)
+//   if (arrAppointments.length === 0) {
+//     return []
+//   }
+//   const appointments = arrAppointments[0].interviewers.map(x => state.interviewers[x])
+//   return appointments
+// }
+
+export function getAppointmentsForDay (state, day) {
+  const appointmentFound = state.days.find (findApp => findApp.name === day)
+  // console.log('appointmentFound', appointmentFound)
+  if(appointmentFound) {
+    // console.log(appointmentFound.appointments.map(x => state.appointments[x]))
+    return appointmentFound.appointments.map(x => state.appointments[x])
+  } else {
     return []
   }
-  const arrAppointments = state.days.filter(dayFilter => dayFilter.name === day)
-  if (arrAppointments.length === 0) {
-    return []
-  }
-  const appointments = arrAppointments[0].appointments.map(x => state.appointments[x])
-  return appointments
 }
 
 export function getInterviewersForDay (state, day) {
-  if (state.days.length === 0) {
+  const appointmentFound = state.days.find (findApp => findApp.name === day)
+  // console.log('appointmentFound', appointmentFound)
+  if(appointmentFound) {
+    // console.log(appointmentFound.appointments.map(x => state.appointments[x]))
+    return appointmentFound.interviewers.map(x => state.interviewers[x])
+  } else {
     return []
   }
-  const arrAppointments = state.days.filter(dayFilter => dayFilter.name === day)
-  if (arrAppointments.length === 0) {
-    return []
-  }
-  const appointments = arrAppointments[0].interviewers.map(x => state.interviewers[x])
-  return appointments
 }
+
 
 export function getInterview(state, interview) {
   if (interview) {
