b49129abc2633684271f7af1eda86bf0121e88c1
cniscoding <asdfa.com>
2023-03-29 09:46:41 -0700

    install and run jest test

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,6 +2,19 @@ import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "./InterviewerList.scss"
 
+// add props-types for teting
+import PropTypes from 'prop-types'; 
+
+// function InterviewerList(props) {
+//   ...
+// }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+// export default InterviewerList;
+
 export default function InterviewerList(props) {
   const interviewList = props.interviewers.map((interview) => {
     return (
